76a5a88ffab1570974664e07414a2b4afc1cd373
Ondrej Mirtes <ondrej@mirtes.cz>
2020-05-14 13:54:31 +0200

    Case-insensitiveness

--- src/Reflector/ClassReflector.php
+++ src/Reflector/ClassReflector.php
@@ -12,6 +12,7 @@ use Roave\BetterReflection\Reflector\Exception\IdentifierNotFound;
 use Roave\BetterReflection\SourceLocator\Type\SourceLocator;
 use function array_key_exists;
 use function assert;
+use function strtolower;
 
 class ClassReflector implements Reflector
 {
@@ -35,14 +36,15 @@ class ClassReflector implements Reflector
      */
     public function reflect(string $className) : Reflection
     {
-        if (array_key_exists($className, $this->cachedReflections)) {
-            $classInfo = $this->cachedReflections[$className];
+        $lowerClassName = strtolower($className);
+        if (array_key_exists($lowerClassName, $this->cachedReflections)) {
+            $classInfo = $this->cachedReflections[$lowerClassName];
         } else {
             $identifier = new Identifier($className, new IdentifierType(IdentifierType::IDENTIFIER_CLASS));
 
             $classInfo = $this->sourceLocator->locateIdentifier($this, $identifier);
             assert($classInfo instanceof ReflectionClass || $classInfo === null);
-            $this->cachedReflections[$className] = $classInfo;
+            $this->cachedReflections[$lowerClassName] = $classInfo;
         }
 
         if ($classInfo === null) {
